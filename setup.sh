67a7f222e35c78996b3280656704b7a42527ea09
Vit Rozkovec <vit.rozkovec@gmail.com>
2013-07-17 14:01:25 +0200

    update repos fix

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 # repo for latest git
 sudo add-apt-repository ppa:pdoes/ppa
-sudo apt-get update
-
 
 # emacs24 repo
 # https://launchpad.net/~cassou/+archive/emacs
 sudo apt-add-repository -y ppa:cassou/emacs
+
+sudo apt-get update
 
 # Install environment
 sudo apt-get install -y git bash-completion emacs24 emacs24-el emacs24-common-non-dfsg
